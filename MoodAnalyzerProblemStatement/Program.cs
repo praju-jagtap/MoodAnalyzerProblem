c5b980f20c2d78d3edc2c907fde4e25bcb4dde89
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-27 18:38:48 +0530

    [Prajakta] Add: Display Welcome Message

---
+++ MoodAnalyzerProblemStatement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyzerProblemStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Mood Analyzer Problem Statement");
+        }
+    }
+}
