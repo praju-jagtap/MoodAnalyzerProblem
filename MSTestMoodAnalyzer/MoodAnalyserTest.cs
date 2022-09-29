251e8324bc982ed2b8517c091a490319685914b3
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-29 19:23:55 +0530

    [Prajakta] Add: Refactor the code should return SAD message

--- MSTestMoodAnalyzer/MoodAnalyserTest.cs
+++ MSTestMoodAnalyzer/MoodAnalyserTest.cs
@@ -6,20 +6,12 @@ namespace MSTestMoodAnalyzer
     public class MoodAnalyserTest
     {
         [TestMethod]
-        public void GivenIAmInSadMood_ShouldReturnSad()
+        public void GivenIAmInSadMessageInConstructor_ShouldReturnSad()
         {
             string message = "I am in Sad Mood";
             MoodAnalyser moodAnalyser = new MoodAnalyser();
             string actual = moodAnalyser.Analyser(message);
             Assert.AreEqual(actual, "SAD");
-        }
-        [TestMethod]
-        public void GivenIAmInAnyMood_ShouldReturnHappy()
-        {
-            string message = "I am in Any Mood";
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-            string actual = moodAnalyser.Analyser(message);
-            Assert.AreEqual(actual, "HAPPY");
         }
     }
 }
