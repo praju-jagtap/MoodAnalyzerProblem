db94988724f210d953b185e32012a40e2ed7c3b2
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-10-02 14:35:00 +0530

    [Prajakta] Add: Use Reflection for Parameterized Constructor

--- MoodAnalyzerProblemStatement/MoodAnalyserFactory.cs
+++ MoodAnalyzerProblemStatement/MoodAnalyserFactory.cs
@@ -10,6 +10,7 @@ namespace MoodAnalyzerProblemStatement
 {
     public class MoodAnalyserFactory
     {
+        //UC4
         public static object CreateMoodAnalyse(string className,string constructorName)
         {
             string pattern = @"." + constructorName + "$";
@@ -30,6 +31,29 @@ namespace MoodAnalyzerProblemStatement
             else
             {
                 throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
+            }
+        }
+
+        //UC5
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName)
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { "HAPPY" });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.EMPTY_MOOD, "Constructor is Not Found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NULL_MOOD, "Class Not Found");
             }
         }
     }
